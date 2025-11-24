f2409a80c6ca8ad4d176d20819084d041928273f
Arthur-Eu-mesmo <arthur.andrade.23lima@gmail.com>
2025-11-24 20:48:54 +0000

    tela de cadastro

--- tela_cadastro.py
+++ tela_cadastro.py
@@ -1,27 +1,26 @@
-# tela de cadastro
-#biblioteca tkinter sendo usado para a criação da interface gráfica
-from tkinter import * 
-#codigo para criar a janela principal
-Jenela = Tk()
-Jenela.title("Tela de Cadastro")
-Jenela.geometry("400x300")
-Inomrações_iniciais = Label(Jenela, text="Realize seu cadastro preenchendo os campos abaixo:")
-Inomrações_iniciais.grid(column=0, row=0, padx=10, pady=10)
-#botão para cadastrar novo usuario
-botao_cadastrar = Button(Jenela, text="Cadastrar Novo Usuario", command=cadastrar_usuario)
-botao_cadastrar.grid(column=0, row=2, padx=10, pady=10)
-texto_botao_cadastrar = Label(Jenela, text="Deseja cadastrar um novo usuario?")
-texto_botao_cadastrar.grid(column=0, row=3, padx=10, pady=10)
-def cadastrar_usuario():
-    Jenela.destroy()
-    import tela_cadastro
-#botão para fazer login
-botao_login = Button(Jenela, text="Fazer Login", command=fazer_login)
-botao_login.grid(column=0, row=4, padx=10, pady=10)
-texto_botao_login = Label(Jenela, text="Ja possui uma conta?")
-texto_botao_login.grid(column=0, row=5, padx=10, pady=10)
+import customtkinter as ctk
+# configurações de aparência
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("dark-blue")
+# tela de login
+tela = ctk.CTk()
+tela.title("Tela de cadastro")
+tela.geometry("400x300")
+informacoes_iniciais = ctk.CTkLabel(tela, text="Faça cadastro preenchendo os campos abaixo:")
+informacoes_iniciais.grid(column=0, row=0, padx=10, pady=10)
+# campo de entrada para o nome de usuário
+entrada_usuario = ctk.CTkEntry(tela, placeholder_text="Nome de Usuário")
+entrada_usuario.grid(column=0, row=1, padx=10, pady=10)
+# campo de entrada para a senha
+entrada_senha = ctk.CTkEntry(tela, placeholder_text="Senha", show="*")
+entrada_senha.grid(column=0, row=2, padx=10, pady=10)
+# botão para fazer cadastro
 def fazer_login():
-    Jenela.destroy()
-    import tela_login
-#codigo para manter a janela aberta
-Jenela.mainloop()
+    usuario = entrada_usuario.get()
+    senha = entrada_senha.get()
+    # aqui você pode adicionar a lógica para verificar o login
+    print(f"Tentando fazer cadastro com Usuário: {usuario} e Senha: {senha}")
+botao_login = ctk.CTkButton(tela, text="Fazer cadastro", command=fazer_login)
+botao_login.grid(column=0, row=3, padx=10, pady=10)
+# código para manter a janela aberta
+tela.mainloop()
