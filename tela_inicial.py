329155220f31bfcc0e3832ca3f0321444fab26d9
Jj-604 <11252101602@alunos.umc.br>
2025-11-24 22:36:44 +0000

    Correções tela_inicial

--- tela_inicial.py
+++ tela_inicial.py
@@ -8,20 +8,20 @@ Jenela.geometry("1920x1080")
 Inomrações_iniciais = Label(Jenela, text="Deseja cadastrar um novo usuario ou fazer login?")
 Inomrações_iniciais.grid(column=0, row=0, padx=10, pady=10)
 #botão para cadastrar novo usuario
+def cadastrar_usuario():
+    Jenela.destroy()
+    import tela_cadastro
 botao_cadastrar = Button(Jenela, text="Cadastrar Novo Usuario", command=cadastrar_usuario)
 botao_cadastrar.grid(column=0, row=2, padx=10, pady=10)
 texto_botao_cadastrar = Label(Jenela, text="Deseja cadastrar um novo usuario?")
 texto_botao_cadastrar.grid(column=0, row=3, padx=10, pady=10)
-def cadastrar_usuario():
-    Jenela.destroy()
-    import tela_cadastro
 #botão para fazer login
+def fazer_login():
+    Jenela.destroy()
+    import tela_login
 botao_login = Button(Jenela, text="Fazer Login", command=fazer_login)
 botao_login.grid(column=0, row=4, padx=10, pady=10)
 texto_botao_login = Label(Jenela, text="Ja possui uma conta?")
 texto_botao_login.grid(column=0, row=5, padx=10, pady=10)
-def fazer_login():
-    Jenela.destroy()
-    import tela_login
 #codigo para manter a janela aberta
 Jenela.mainloop()
